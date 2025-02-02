ec832b355d75a72c16bbf71f1581202d6963329b
butler <113191739+SchrodingersMouser@users.noreply.github.com>
2025-02-01 20:11:18 -0600

    write stopwatch function (glitches when function is re-run)

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,25 +1,49 @@
 <script>
+    let hours = 0;
     let minutes = 12;
     let seconds = 30;
+    let centiseconds = 0;
 
+    const now = Date();
+
+    function stopwatch (){
+        let start = new Date();
+
+        setInterval(updateElapsed, 10)
+
+        function updateElapsed () {
+            const current = new Date();
+            let elapsed = current - start;
+            updateDisplay(elapsed);
+            
+            function updateDisplay (total) {
+                let remainder = total;
+                hours = Math.floor(total/(3.6 * 1000000)).toLocaleString('en-US', {minimumIntegerDigits: 2, useGrouping:false});
+                minutes = Math.floor((total%(3.6 * 1000000))/60000).toLocaleString('en-US', {minimumIntegerDigits: 2, useGrouping:false});
+                seconds = Math.floor(((total%(3.6 * 1000000))%60000)/1000).toLocaleString('en-US', {minimumIntegerDigits: 2, useGrouping:false});
+                centiseconds = Math.floor((((total%(3.6 * 1000000))%60000)%1000)/10);
+            }
+        }
+    }
+
+    
 </script>
 <main>
+    <div class = "header">
+        <div> {now} </div>
+    </div>
     <div class="clockDisplay">
         <div class="numbers">
-            {minutes}:{seconds}
+            {hours}:{minutes}:{seconds}.{centiseconds}
         </div>
     </div>
     <div class="controls">
-        
+        <button on:click={stopwatch}>start</button>
     </div>
     
 </main>
 
 <style>
-    body{
-        padding: 0;
-        margin: 0;
-    }
     main{
         background-color: grey;
         font-family: 'Roboto Mono', monospace;
