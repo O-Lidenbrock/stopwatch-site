a6afd31738beb0b38d3675ee80b9f5020b5c0828
butler <113191739+SchrodingersMouser@users.noreply.github.com>
2025-02-12 14:36:40 -0600

    implement pause and stop, center display, format numbers in seperate divs

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,23 +2,23 @@
     import { updated } from "$app/state";
 
     let milliseconds = $state(0);
-    let hours = $derived(Math.floor(milliseconds/(3.6 * 1000000)).toLocaleString('en-US', {minimumIntegerDigits: 2, useGrouping:false}));
+    let hours = $derived(Math.floor(milliseconds/(3.6 * 1000000)));
     let minutes = $derived(Math.floor((milliseconds%(3.6 * 1000000))/60000).toLocaleString('en-US', {minimumIntegerDigits: 2, useGrouping:false}));
     let seconds = $derived(Math.floor(((milliseconds%(3.6 * 1000000))%60000)/1000).toLocaleString('en-US', {minimumIntegerDigits: 2, useGrouping:false}));
     let centiseconds = $derived(Math.floor((((milliseconds%(3.6 * 1000000))%60000)%1000)/10).toLocaleString('en-US', {minimumIntegerDigits: 2, useGrouping:false}));
 
     const now = Date.now();
-    let paused = 0;
+    let paused = $state(0);
     let timerID = null;
 
     function stopwatch(startTime, ms){
         milliseconds = ms;
         let start = startTime;
 
-        timerID = setInterval(update, 100);
+        timerID = setInterval(update, 50);
 
         function update(){
-            milliseconds = Date.now() - startTime;
+            milliseconds = Date.now() - startTime + paused;
         }
     }
 
@@ -37,13 +37,14 @@
     }
 
     function pause(){
-        clearInterval(timerID);
         paused = milliseconds;
+        clearInterval(timerID);
     }
     
     function stop(){
         clearInterval(timerID);
         milliseconds = 0;
+        paused = 0;
     }
 
     
@@ -54,7 +55,13 @@
     </div>
     <div class="clockDisplay">
         <div class="numbers">
-            {hours}:{minutes}:{seconds}.{centiseconds}
+            <div class="hour">{hours}</div>
+            <div class="hour">:</div>
+            <div class="minute">{minutes}</div>
+            <div class="colon">:</div>
+            <div class="second">{seconds}</div>
+            <div class="period">.</div>
+            <div class="centisecond">{centiseconds}</div>
         </div>
     </div>
     <div class="controls">
@@ -62,7 +69,8 @@
         <button onclick={pause}>pause</button>
         <button onclick={stop}>stop</button>
     </div>
-    <div>"Hello there"</div>
+    <div>current ms: {milliseconds}</div>
+    <div>"current paused: {paused}</div>
     
 </main>
 
@@ -70,19 +78,27 @@
     main{
         background-color: grey;
         font-family: 'Roboto Mono', monospace;
-        margin: 0;
         width: 100%;
         height: 100;
     }
     .clockDisplay{
-        display:flex;
-        align-items: center;
-        justify-content: center;
+        max-width: fit-content;
+        margin-right: auto;
+        margin-left: auto;
     }
     .numbers{
+        display: flex;
+        flex-direction: row;
         font-size: 100px;
         color: rgb(17, 13, 8);
-        width: 50px;
+        align-items: baseline;
+    }
+    .centisecond{
+        font-size: 50px;
+        color: goldenrod;
+    }
+    .hour{
+        color: rgb(80, 77, 77);
     }
 </style>
 
