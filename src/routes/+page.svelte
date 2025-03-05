23daef3e60c55a8eef479f1c9f1895c52834a522
butler <113191739+SchrodingersMouser@users.noreply.github.com>
2025-03-05 10:15:50 -0600

    it's responsive now

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -13,6 +13,7 @@
     let lapStart;
     let paused = $state(0);
     let timerID = null;
+    let running = $state(false);
 
     const stopwatch = (startTime, ms) => {
         milliseconds = ms;
@@ -29,6 +30,7 @@
         const start = Date.now();
         lapStart = Date.now();
         let ms = paused;
+        running = true;
 
         if(!(timerID === null)){
             clearInterval(timerID)
@@ -44,11 +46,13 @@
     }
 
     const pause = () => {
+        running = false;
         paused = milliseconds;
         clearInterval(timerID);
     }
     
     const stop = () => {
+        running = false
         clearInterval(timerID);
         milliseconds = 0;
         paused = 0;
@@ -83,30 +87,42 @@
 
     }
 
+    const onKeyDown = (e) => {
+        switch(e.keyCode) {
+            case 32:{
+                if(!running){
+                    play();
+                }
+                else{
+                    pause();
+                }}
+        }
+    }
+
 
 </script>
 <main>
-    <div class = "header">
-        <div> {now} </div>
-    </div>
     <div class="clockDisplay">
         <div class="numbers">
-            <div class="hour">{hours}</div>
-            <div class="hour">:</div>
+            {#if hours != 0}
+                <div class="hour">{hours}:</div>
+            {/if}
             <div class="minute">{minutes}</div>
-            <div class="colon">:</div>
+            <div class="minute" id="colon">:</div>
             <div class="second">{seconds}</div>
-            <div class="period">.</div>
-            <div class="centisecond">{centiseconds}</div>
+            <div class="centisecond">.{centiseconds}</div>
         </div>
     </div>
     <div class="controls">
+        {#if !running}
         <button class="controlBtn" id="Play" onclick={play} aria-label="Play"> <!--- wrote this with ai bc I had no idea what was standard for this sorta thing --->
             <img src="icons/play-icon.svg" alt="Play">
         </button>
+        {:else}
         <button class="controlBtn" onclick={pause} aria-label="Pause">
             <img src="icons/pause-icon.svg" alt="Pause">
         </button>
+        {/if}
         <button class="controlBtn" onclick={restart} aria-label="Restart">
             <img src="icons/restart-icon.svg" alt="Restart">
         </button>
@@ -114,20 +130,18 @@
             <img src="icons/lap2-icon.svg" alt="Lap">
         </button>
        </div>
-    <div>current ms: {milliseconds}</div>
-    <div>"current paused: {paused}</div>
     <div class="laps"></div>
     
 </main>
 
 <style>
     main{
-        background-color: grey;
         font-family: 'Roboto Mono', monospace;
         width: 100%;
         height: 100;
+        color: rgb(157, 155, 150);
     }
-    .clockDisplay, .controls{
+    .clockDisplay, .controls, .laps{
         max-width: fit-content;
         margin-right: auto;
         margin-left: auto;
@@ -136,16 +150,57 @@
     .numbers{
         display: flex;
         flex-direction: row;
-        font-size: 100px;
-        color: rgb(17, 13, 8);
+        flex-wrap: wrap;
+        padding: 20px;
+        font-size: clamp(80px, 15vw, 120px);
+        color: rgb(136, 132, 126);
         align-items: baseline;
-    }
-    .centisecond{
-        font-size: 50px;
-        color: goldenrod;
     }
     .hour{
         color: rgb(80, 77, 77);
+    }  
+    .centisecond{
+        font-size: 50px;
+        color: rgb(138, 107, 27);
+    }
+    button{
+        background-color: rgb(54, 109, 156);
+        border-radius: 50%;
+        border-color: black;
+        padding: 3px;
+        aspect-ratio: 1/1;        
+    }
+
+    .laps{
+        display: flex;
+        flex-direction: column;
+        gap: 8px;
+    }
+    .controls{
+        display: flex;
+        justify-content: center;
+        gap: 16px;
+        flex-wrap: wrap;
+    }
+
+    @media (max-width: 350px) {
+        .centisecond{
+            display: none;
+        }
+    }
+    @media (max-width: 260px) {
+        #colon{
+            display: none;
+        }
+        .minute, .second{
+            font-size: 100px;
+            margin-right: auto;
+            margin-left: auto;
+        }
+        .second{
+            margin-top: -30px;
+        }
     }
 </style>
 
+<svelte:window on:keydown|preventDefault={onKeyDown} />
